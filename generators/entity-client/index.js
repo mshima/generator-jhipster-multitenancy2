77d600605fea6f6c33e1c143b37d429bade64ee2
Marcelo Boveto Shima <marcelo@shimasolucoes.com.br>
2019-08-30 13:26:01 -0300

    Cleanup entity-client an use Spread syntax

--- generators/entity-client/index.js
+++ generators/entity-client/index.js
@@ -8,85 +8,33 @@ const workarounds = require('../workarounds');
 module.exports = class extends EntityClientGenerator {
     constructor(args, opts) {
         super(args, { fromBlueprint: true, ...opts }); // fromBlueprint variable is important
-
-        //        const jhContext = (this.jhipsterContext = this.options.jhipsterContext);
-
-        //        if (!this.jhipsterContext) {
-        //            this.error(`This is a JHipster blueprint and should be used only like ${chalk.yellow('jhipster --blueprint multitenancy2')}`);
-        //        }
-
-        //        this.configOptions = jhContext.configOptions || {};
-
-        // This sets up options for this sub generator and is being reused from JHipster
-        //        jhContext.setupEntityOptions(this, jhContext, this);
     }
 
     get initializing() {
-        /**
-         * Any method beginning with _ can be reused from the superclass `EntityClientGenerator`
-         *
-         * There are multiple ways to customize a phase from JHipster.
-         *
-         * 1. Let JHipster handle a phase, blueprint doesnt override anything.
-         * ```
-         *      return super._initializing();
-         * ```
-         *
-         * 2. Override the entire phase, this is when the blueprint takes control of a phase
-         * ```
-         *      return {
-         *          myCustomInitPhaseStep() {
-         *              // Do all your stuff here
-         *          },
-         *          myAnotherCustomInitPhaseStep(){
-         *              // Do all your stuff here
-         *          }
-         *      };
-         * ```
-         *
-         * 3. Partially override a phase, this is when the blueprint gets the phase from JHipster and customizes it.
-         * ```
-         *      const phaseFromJHipster = super._initializing();
-         *      const myCustomPhaseSteps = {
-         *          displayLogo() {
-         *              // override the displayLogo method from the _initializing phase of JHipster
-         *          },
-         *          myCustomInitPhaseStep() {
-         *              // Do all your stuff here
-         *          },
-         *      }
-         *      return Object.assign(phaseFromJHipster, myCustomPhaseSteps);
-         * ```
-         */
-        const phaseFromJHipster = super._initializing();
-        const myCustomPhaseSteps = {
+        const postInitializingSteps = {
             prepare() {
                 if (this.isTenant) {
                     workarounds.fixAddEntityToModule(this);
                 }
             }
         };
-        return Object.assign(phaseFromJHipster, myCustomPhaseSteps);
+        return { ...super._initializing(), ...postInitializingSteps };
     }
 
     get prompting() {
-        // Here we are not overriding this phase and hence its being handled by JHipster
         return super._prompting();
     }
 
     get configuring() {
-        // Here we are not overriding this phase and hence its being handled by JHipster
         return super._configuring();
     }
 
     get default() {
-        // Here we are not overriding this phase and hence its being handled by JHipster
         return super._default();
     }
 
     get writing() {
-        const phaseFromJHipster = super._writing();
-        const myCustomPhaseSteps = {
+        const postWritingSteps = {
             generateClientCode() {
                 mtUtils.processPartialTemplates(files.angular.angularTemplates(this), this);
                 if (this.isTenant) {
@@ -123,16 +71,14 @@ module.exports = class extends EntityClientGenerator {
                 }
             }
         };
-        return Object.assign(phaseFromJHipster, myCustomPhaseSteps);
+        return { ...super._writing(), ...postWritingSteps };
     }
 
     get install() {
-        // Here we are not overriding this phase and hence its being handled by JHipster
         return super._install();
     }
 
     get end() {
-        // Here we are not overriding this phase and hence its being handled by JHipster
         return super._end();
     }
 };
