5d0364bdb9320a868b2085c83f0ed1267fe875c4
Marcelo Boveto Shima <marcelo@shimasolucoes.com.br>
2019-09-03 14:47:11 -0300

    Try to fix blueprint test

--- test-integration/scripts/10.1-install-jhipster-blueprint.sh
+++ test-integration/scripts/10.1-install-jhipster-blueprint.sh
@@ -9,7 +9,6 @@ source $(dirname $0)/00-init-env.sh
 cd "$HOME"
 echo "*** generator-jhipster-blueprint: use local version at JHI_REPO=$JHI_REPO"
 
-cd "$JHI_HOME"
 git --no-pager log -n 10 --graph --pretty='%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit
 
 npm install -g "$JHI_HOME"
