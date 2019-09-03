ff9533c8f81e136bd00b23bcc051f59ebadd2478
Marcelo Boveto Shima <marcelo@shimasolucoes.com.br>
2019-09-03 15:04:39 -0300

    Pipeline: Create folder.

--- test-integration/scripts/10.1-install-jhipster-blueprint.sh
+++ test-integration/scripts/10.1-install-jhipster-blueprint.sh
@@ -12,6 +12,9 @@ echo "*** generator-jhipster-blueprint: use local version at JHI_REPO=$JHI_REPO"
 cd "$JHI_HOME"
 git --no-pager log -n 10 --graph --pretty='%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit
 
+rm -rf "$JHI_FOLDER_APP"
+mkdir -p "$JHI_FOLDER_APP"
 cd "$JHI_FOLDER_APP"
+
 npm install "$JHI_HOME"
 npm test
