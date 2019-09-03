b20fa65e42ee27b9be8d227de13dfcb56f84c4cb
Marcelo Boveto Shima <marcelo@shimasolucoes.com.br>
2019-09-03 15:30:16 -0300

    Initialize npm to workaround error
    https://github.com/visionmedia/debug/issues/261

--- test-integration/scripts/10.1-install-jhipster-blueprint.sh
+++ test-integration/scripts/10.1-install-jhipster-blueprint.sh
@@ -16,5 +16,11 @@ rm -rf "$JHI_FOLDER_APP"
 mkdir -p "$JHI_FOLDER_APP"
 cd "$JHI_FOLDER_APP"
 
+# Create package.json otherwise npm will error and break
+npm init -f
+
 npm install "$JHI_HOME"
 npm test
+
+# Remove package.json to let jhipster create it's own
+rm package.json
