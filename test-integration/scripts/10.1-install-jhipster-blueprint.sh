03fe3424be7a2f24e3a64a6148dae6e0924973ae
Marcelo Boveto Shima <marcelo@shimasolucoes.com.br>
2019-09-03 14:56:08 -0300

    Try a blueprint local installation

--- test-integration/scripts/10.1-install-jhipster-blueprint.sh
+++ test-integration/scripts/10.1-install-jhipster-blueprint.sh
@@ -9,7 +9,9 @@ source $(dirname $0)/00-init-env.sh
 cd "$HOME"
 echo "*** generator-jhipster-blueprint: use local version at JHI_REPO=$JHI_REPO"
 
+cd "$JHI_HOME"
 git --no-pager log -n 10 --graph --pretty='%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit
 
-npm install -g "$JHI_HOME"
+cd "$JHI_FOLDER_APP"
+npm install "$JHI_HOME"
 npm test
