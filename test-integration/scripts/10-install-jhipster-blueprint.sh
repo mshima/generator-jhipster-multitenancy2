3ee8b84f96af33a2d6425d758c1096bf14f50b96
Marcelo Boveto Shima <marcelo@shimasolucoes.com.br>
2019-09-03 14:14:39 -0300

    Install blueprint on azure pipeline

---
+++ test-integration/scripts/10-install-jhipster-blueprint.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+set -e
+source $(dirname $0)/00-init-env.sh
+
+#-------------------------------------------------------------------------------
+# Install Blueprint
+#-------------------------------------------------------------------------------
+cd "$HOME"
+echo "*** generator-jhipster-blueprint: use local version at JHI_REPO=$JHI_REPO"
+
+cd "$JHI_HOME"
+git --no-pager log -n 10 --graph --pretty='%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit
+
+npm install -g "$JHI_HOME"
+npm test
--- test-integration/scripts/12-generate-project.sh
+++ test-integration/scripts/12-generate-project.sh
@@ -29,7 +29,7 @@ elif [[ "$JHI_ENTITY" == "jdl-multitenancy2-company" ]]; then
     cd "$JHI_FOLDER_APP"
     jhipster import-jdl *.jdl --no-insight --blueprints multitenancy2 --tenant-name company --relation-tenant-aware
     # Regenerate to fix errors
-    jhipster --with-entities --no-insight --blueprints multitenancy2 --relation-tenant-aware --force
+    jhipster --force --no-insight --skip-checks --with-entities --from-cli --blueprints multitenancy2 --relation-tenant-aware
 
 else
     #-------------------------------------------------------------------------------
