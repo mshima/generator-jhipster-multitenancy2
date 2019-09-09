92f9e57afe41cfa3cb94e6cb39445a2b6da9612c
Marcelo Boveto Shima <marcelo@shimasolucoes.com.br>
2019-09-09 14:50:20 -0300

    Add condition to navbar patch

--- generators/entity-client/partials/tenant_hide_if_exists_on_account/navbar.component.html.js
+++ generators/entity-client/partials/tenant_hide_if_exists_on_account/navbar.component.html.js
@@ -3,6 +3,7 @@ const file = context => `${context.CLIENT_MAIN_SRC_DIR}app/layouts/navbar/navbar
 const tmpls = [
     {
         // Add condition to hide menu
+        condition: context => context.isTenant,
         type: 'replaceContent',
         regex: false,
         target: context => new RegExp(`<li>\n(\\s*)(<a class="dropdown-item" routerLink="${context.tenantStateName}")`),
--- generators/entity-client/partials/tenant_move_to_admin_menu/navbar.component.html.js
+++ generators/entity-client/partials/tenant_move_to_admin_menu/navbar.component.html.js
@@ -4,6 +4,7 @@ const tmpls = [
     {
         // https://github.com/jhipster/generator-jhipster/pull/10155
         // Fixed on 6.2.1
+        condition: context => context.isTenant,
         type: 'replaceContent',
         versions: ['6.1.2', '6.2.0'],
         regex: false,
