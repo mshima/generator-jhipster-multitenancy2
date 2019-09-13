ebae70113905de9eb21c9457a50f8fd9511cef71
Marcelo Boveto Shima <marcelo@shimasolucoes.com.br>
2019-09-13 15:07:12 -0300

    Add order on navbar (6.2.0)

--- generators/client/patcher/files.v6.2.0.js
+++ generators/client/patcher/files.v6.2.0.js
@@ -4,8 +4,20 @@ module.exports = {
     files: {
         user_management_module: [
             {
+                version: '6.2.0',
                 path: jhipsterConstants.ANGULAR_DIR,
                 templates: ['admin/user-management/user-management.module.ts']
+            }
+        ],
+        tenant_admin_menu: [
+            {
+                path: jhipsterConstants.ANGULAR_DIR,
+                templates: [
+                    {
+                        file: 'layouts/navbar/navbar.component.html.v6.2.0',
+                        renameTo: () => 'layouts/navbar/navbar.component.html'
+                    }
+                ]
             }
         ]
     }
