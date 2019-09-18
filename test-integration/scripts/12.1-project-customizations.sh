9138da75f29025bbd4d7a58667d5a218b670e162
Marcelo Boveto Shima <marcelo@shimasolucoes.com.br>
2019-09-18 17:38:25 -0300

    Fix integration test

--- test-integration/scripts/12.1-project-customizations.sh
+++ test-integration/scripts/12.1-project-customizations.sh
@@ -9,17 +9,15 @@ source $(dirname $0)/00-init-env.sh
 
 cd "$JHI_FOLDER_APP"
 
+# Replace version (latest) with local path
+echo 'Changing blueprint version'
+echo `cat package.json | grep 'generator-jhipster-multitenancy2'`
+sed -e 's#"generator-jhipster-multitenancy2": ".*",#"generator-jhipster-multitenancy2": "file:'$JHI_HOME'",#1;' package.json > package.json.sed
+mv -f package.json.sed package.json
+echo 'Fixed blueprint version'
+echo `cat package.json | grep 'generator-jhipster-multitenancy2'`
+
+npm install
+
 echo "Updation webdriver from chrome"
 npm install webdriver-manager@12.1.6
-
-# https://github.com/eslint/eslint/issues/11914
-npm install eslint eslint-plugin-import eslint-config-airbnb-base eslint-config-prettier eslint-plugin-prettier
-
-if [[ "$INSTALL_BLUEPRINT" != "global" && "$JHI_GEN_BRANCH" == "release" && "$JHI_GEN_VERSION" == "" ]]; then
-    echo "*** generator-jhipster: use last release version"
-    npm install generator-jhipster
-
-elif [[ "$INSTALL_BLUEPRINT" != "global" && "$JHI_GEN_BRANCH" == "release" ]]; then
-    echo "*** generator-jhipster: use release version"
-    npm install "generator-jhipster@$JHI_GEN_VERSION"
-fi
