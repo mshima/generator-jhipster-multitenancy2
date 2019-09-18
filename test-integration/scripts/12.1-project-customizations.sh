1082a86ef06d45e29d1382bf3ef4a276e854fcc1
Marcelo Boveto Shima <marcelo@shimasolucoes.com.br>
2019-09-18 16:16:50 -0300

    Try to fix eslint problem

--- test-integration/scripts/12.1-project-customizations.sh
+++ test-integration/scripts/12.1-project-customizations.sh
@@ -11,15 +11,9 @@ cd "$JHI_FOLDER_APP"
 
 echo "Updation webdriver from chrome"
 npm install webdriver-manager@12.1.6
-npm install -g eslint eslint-plugin-import eslint-plugin-jsx-a11y
 
-#if [[ "$INSTALL_BLUEPRINT" == "global" ]]; then
-#    echo "Installing eslint"
-#else
-#    echo "Installing eslint"
-#    npm install eslint
-#    npm install eslint
-#fi
+# https://github.com/eslint/eslint/issues/11914
+npm install eslint eslint-plugin-import eslint-config-airbnb-base eslint-config-prettier eslint-plugin-prettier
 
 if [[ "$INSTALL_BLUEPRINT" != "global" && "$JHI_GEN_BRANCH" == "release" && "$JHI_GEN_VERSION" == "" ]]; then
     echo "*** generator-jhipster: use last release version"
