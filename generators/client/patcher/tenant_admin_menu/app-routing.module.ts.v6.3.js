a3a0c7bd2d586dd593a63c293d99927bd9b2a042
Marcelo Boveto Shima <marcelo@shimasolucoes.com.br>
2019-09-23 21:53:39 -0300

    Create app-routing.module.ts.v6.3.js

---
+++ generators/client/patcher/tenant_admin_menu/app-routing.module.ts.v6.3.js
@@ -0,0 +1,16 @@
+const file = context => `${context.CLIENT_MAIN_SRC_DIR}app/app-routing.module.ts`;
+
+const tmpls = [
+    {
+        versions: ['6.3.0', '6.3.1'],
+        type: 'rewriteFile',
+        target: context => 'const LAYOUT_ROUTES = [',
+        tmpl: context => `import { UserRouteAccessService } from 'app/core/auth/user-route-access-service';
+`
+    }
+];
+
+module.exports = {
+    file,
+    tmpls
+};
