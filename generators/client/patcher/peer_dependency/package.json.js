cfb24371e98102aa64a1f3f59fd726e3533807e8
Marcelo Boveto Shima <marcelo@shimasolucoes.com.br>
2019-10-18 16:28:44 -0300

    Move modules/blueprints to peerDependencies too.

--- generators/client/patcher/peer_dependency/package.json.js
+++ generators/client/patcher/peer_dependency/package.json.js
@@ -2,18 +2,21 @@ const file = context => 'package.json';
 
 const tmpls = [
     {
+        // Create peerDependencies
         type: 'replaceContent',
-        target: /(\s*"generator-jhipster": "[\w.]*"),(.*)(\n\s*)("engines": \{)/is,
-        tmpl: '$2$3"peerDependencies": {$1$3},$3$4'
-    }
-    /*
+        target: /((\s*)"engines": \{)/,
+        tmpl: '$2"peerDependencies": {$2},$1'
+    },
     {
         type: 'replaceContent',
-        ignorePatchErrors: true,
-        target: /("typescript": "[\w.]*",)(.*)(\n\s*"webdriver-manager": "[\w.]*")/is,
-        tmpl: ''
+        target: /(\s*"generator-jhipster": "[\w.]*"),(.*"peerDependencies": \{)/s,
+        tmpl: '$2$1'
+    },
+    {
+        type: 'replaceContent',
+        target: /("typescript": "[\w.]*",)(.*),(\n\s*"webdriver-manager": "[\w.]*".*"generator-jhipster": "[\w.]*")/s,
+        tmpl: '$1$3,$2'
     }
-    */
 ];
 
 module.exports = {
