4068532ab3c335b280d34813fe15c154cd0d0c7b
Marcelo Boveto Shima <marcelo@shimasolucoes.com.br>
2019-10-18 15:58:03 -0300

    Print generator-jhipster version found by yeoman-environment.

--- generators/jhipster-environment.js
+++ generators/jhipster-environment.js
@@ -1,11 +1,15 @@
 const Env = require('yeoman-environment');
 const path = require('path');
+const chalk = require('chalk');
 
 const generatorPath = Env.lookupGenerator('jhipster:app');
 const generatorsPath = path.dirname(path.dirname(generatorPath));
 const packagePath = path.dirname(generatorsPath);
 const utils = require(`${generatorsPath}/utils`);
 const constants = require(`${generatorsPath}/generator-constants`);
+const jhipsterVersion = require(`${packagePath}/package.json`).version;
+
+console.log(`\nExtending peer generator-jhipster version ${chalk.yellow(`${jhipsterVersion}`)} at ${chalk.yellow(`${generatorsPath}`)}\n`);
 
 const generator = function(generator) {
     return require(`${generatorsPath}/${generator}`);
