31183c4b0251e5c458bb2688d6de73bab208afa0
Marcelo Boveto Shima <marcelo@shimasolucoes.com.br>
2019-10-21 09:50:22 -0200

    Set version version: '^6.4.0' on settings.component

--- generators/client/patcher/tenant_add_to_account/settings.component.spec.ts.js
+++ generators/client/patcher/tenant_add_to_account/settings.component.spec.ts.js
@@ -7,7 +7,6 @@ const file = context => `${jhipsterConstants.CLIENT_TEST_SRC_DIR}spec/app/accoun
 const tmpls = [
     {
         type: 'replaceContent',
-        regex: true,
         target: /(\n(\s*)imageUrl: '')(,?)/,
         tmpl: context => `$1,
 $2company: 'MockCompany'$3`
