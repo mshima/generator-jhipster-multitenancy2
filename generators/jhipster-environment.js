ad88ced9ba583bd649d233401cf1117bead8672e
Marcelo Boveto Shima <marcelo@shimasolucoes.com.br>
2019-10-29 10:41:50 -0200

    Implement inheritPriorities

--- generators/jhipster-environment.js
+++ generators/jhipster-environment.js
@@ -2,6 +2,8 @@ const Env = require('yeoman-environment');
 const path = require('path');
 const chalk = require('chalk');
 
+const debug = require('debug')('tenancy-view:env');
+
 const autoExtender = require('./auto-extender');
 
 const generatorPath = Env.lookupGenerator('jhipster:app');
@@ -14,7 +16,40 @@ const jhipsterVersion = require(`${packagePath}/package.json`).version;
 console.log(`\nExtending peer generator-jhipster version ${chalk.yellow(`${jhipsterVersion}`)} at ${chalk.yellow(`${generatorsPath}`)}\n`);
 
 const generator = function(generator) {
-    return autoExtender(require(`${generatorsPath}/${generator}`));
+    const original = require(`${generatorsPath}/${generator}`);
+    return class GeneratorExtender extends autoExtender(original) {
+        constructor(args, opts) {
+            super(args, opts);
+
+            if (opts.inherit === undefined || opts.inherit) {
+                this.inheritPriorities(original.prototype);
+            }
+            this.superPrototype = original.prototype;
+        }
+
+        inheritPriorities(prototype = this.superPrototype, opts = {}) {
+            const self = this;
+            let queueNames = opts.queueNames || this.env.runLoop.queueNames;
+            queueNames = opts.force
+                ? queueNames
+                : queueNames.filter(queue => Object.getOwnPropertyDescriptor(Object.getPrototypeOf(self), queue) === undefined);
+
+            debug(queueNames);
+            queueNames = queueNames.filter(queue => {
+                const property = Object.getOwnPropertyDescriptor(prototype, queue);
+                if (property) {
+                    debug(`Queueing phase ${queue}`);
+                    if (property.get) {
+                        self.queueMethod(property.get.call(self), queue);
+                        return false;
+                    }
+                    self.queueMethod(property.value, queue, queue);
+                    return false;
+                }
+                return true;
+            });
+        }
+    };
 };
 
 module.exports = { packagePath, generatorsPath, utils, constants, generator, jhipsterVersion };
